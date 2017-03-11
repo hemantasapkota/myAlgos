1573ffa9730c3d1aac0144fdc3d44fc1a808d7ac
Hemanta Sapkota <laex.pearl@gmail.com>
2017-03-11 19:44:43 +1100

    Add docs

--- main.go
+++ main.go
@@ -6,16 +6,21 @@ import (
 
 func main() {
 
+	//Problem: Detect cycles in a linked list
 	println(ex.ContainsCycle(ex.MakeListWithCycles()))
 
+	//Problem: Find the index of the first leftmost occurrence of k in XS.
 	println(ex.FirstK([]int{5, 5, 3, 4, 5, 5, 5}, 5))
 
-	// Stack based question
+	//Problem: Check if delimeters are matched
 	println(ex.MatchDelimeters("{ [ ( ] ) }"))
 
+	//Problem: Reverse a linked list
 	ex.ReverseLinkedList()
 
+	//Problem: Reverse a stack using recursion
 	ex.ReverseStackUsingRecursion()
 
+	//Problem: Reverse characters in place
 	ex.ReverseCharsInPlace()
 }
